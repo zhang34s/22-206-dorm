f30203714a8ab918af53c7b065791c16dba114ff
zhang34s <zhang34@ucsb.edu>
2026-09-23 04:41:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // anon key 本来就是公开的，放在前端没问题；service_role / secret key 绝对不要放进来。
 window.DORM_CONFIG = {
   supabaseUrl: 'https://rjocfvajtbaigdjgyavg.supabase.co',
-  supabaseAnonKey: 'PASTE_ANON_KEY_HERE',
+  supabaseAnonKey: 'sb_publishable_k7FQC53CKSN1MIrPVnWAtw_Yx_Z2-Rw',
 };
